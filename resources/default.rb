d6356d9f4b443d3d94bdb8e234061ce011b332c8
Andrew Heald <andrew@heald.co.uk>
2016-01-25 17:18:32 +0000

    Add explicit nil handling for group to keep Chef Client happy, re ...
    group nil currently does not overwrite the value of group. This will change in Chef 13, and the value will be set to nil instead. Please change your code to explicitly accept nil using "property :group, [MyType, nil]", or stop setting this value to nil. at 1 location:
        - /var/chef/cache/cookbooks/posix_acl/providers/default.rb:63:in `load_current_resource'

--- resources/default.rb
+++ resources/default.rb
@@ -15,7 +15,7 @@ default_action :create
 attribute :path, :name_attribute => true, :kind_of => String, :required => true
 
 attribute :user       , :kind_of => String
-attribute :group      , :kind_of => String, :default => nil
+attribute :group      , :kind_of => [ String, nil ], :default => nil
 attribute :read       , :kind_of => [ TrueClass, FalseClass ], :default => true
 attribute :write      , :kind_of => [ TrueClass, FalseClass ], :default => false
 attribute :execute    , :kind_of => [ TrueClass, FalseClass ], :default => true
